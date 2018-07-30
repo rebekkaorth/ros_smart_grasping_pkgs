5f9b3700c9a651fc53032b0edae0775dc67b2745
Rebekka <rebekkaorth94@gmail.com>
2018-07-30 10:02:02 +0000

    pick function added

--- object_grasping/src/image_collector.py
+++ object_grasping/src/image_collector.py
@@ -19,55 +19,40 @@ def talker():
         pub.publish(hello_str)
         rate.sleep()
         
-def grasp_ball(): 
+def grasp_ball(x, y, z, roll, pitch, yaw):
+    print(type(grasper.get_object_pose()))
     print(grasper.get_object_pose())
-     
-    rospy.loginfo("Moving to Pregrasp")
-     
+    print("position: {x}, {y}, {z}").format(x=x, y=y, z=z) 
+    print("orientation: {roll}, {pitch}, {yaw}").format(roll=roll, pitch=pitch, yaw=yaw)
+    print("tip pose: {tippose}").format(tippose=grasper.get_tip_pose())
+    
+    rospy.loginfo("opening hand")
     grasper.open_hand()
-    time.sleep(0.1)
-        
-    ball_pose = grasper.get_object_pose()
-    ball_pose.position.x = -0.472 
-    ball_pose.position.y = 0.159
-    ball_pose.position.z = 0.772
-    ball_pose.position.z += 0.5
-        
-    print(ball_pose.position)
-        
-    #setting an absolute orientation (from the top)
-    quaternion = quaternion_from_euler(-pi/2., 0.0, 0.0)
-    ball_pose.orientation.x = quaternion[0]
-    ball_pose.orientation.y = quaternion[1]
-    ball_pose.orientation.z = quaternion[2]
-    ball_pose.orientation.w = quaternion[3]
-        
-    grasper.move_tip_absolute(ball_pose)
-    time.sleep(0.1)
-        
-    rospy.loginfo("Grasping")
-    grasper.move_tip(y=-0.164)
-    time.sleep(0.1)
+    time.sleep(1)
+    
+    rospy.loginfo("move tool tip to object pose")
+    grasper.move_tip(x, y, z, roll, pitch, yaw)
+    time.sleep(1)
+    print("tip pose 2: {tippose}").format(tippose=grasper.get_tip_pose())
+    time.sleep(1)
+    
+    rospy.loginfo("check if hand is open")
     grasper.check_fingers_collisions(False)
-    time.sleep(0.1)
+    time.sleep(1)
+    
+    rospy.loginfo("close hand")
     grasper.close_hand()
-    time.sleep(0.1)
-        
-    rospy.loginfo("Lifting")
-    for _ in range(5):
+    time.sleep(1)
+    
+    rospy.loginfo("lift object")
+    for _ in range(10):
         grasper.move_tip(y=0.01)
         time.sleep(0.1)
-            
-    grasper.check_fingers_collisions(True)
-     
-     
-    grasper.open_hand()
-    
-    
+        
         
 if __name__ == '__main__':
     try:
-        grasp_ball()
+        grasp_ball(-0.472, 0.159, 0.772, 0.001, -0.001, 0)
         
     except rospy.ROSInterruptException:
         pass
