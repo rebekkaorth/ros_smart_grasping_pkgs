683b6733e92f341bbe2de7c7f28db25c1b9f99a5
Rebekka <rebekkaorth94@gmail.com>
2018-07-30 11:39:11 +0000

    pick up function adjusted

--- object_grasping/src/image_collector.py
+++ object_grasping/src/image_collector.py
@@ -4,8 +4,9 @@ import rospy
 from std_msgs.msg import String
 from smart_grasping_sandbox.smart_grasper import SmartGrasper
 import time
+from geometry_msgs.msg import Pose
 from tf.transformations import quaternion_from_euler
-from math import pi
+from math import pi, cos, sin
 
 grasper = SmartGrasper()
 
@@ -26,12 +27,34 @@ def grasp_ball(x, y, z, roll, pitch, yaw):
     print("orientation: {roll}, {pitch}, {yaw}").format(roll=roll, pitch=pitch, yaw=yaw)
     print("tip pose: {tippose}").format(tippose=grasper.get_tip_pose())
     
+    object_pose = Pose()
+    
+    object_pose.position.x = x
+    object_pose.position.y = y
+    object_pose.position.z = z
+    
+    object_pose.position.z += 0.5
+    
+    object_pose.orientation.x = cos(yaw) * cos(pitch)
+    object_pose.orientation.y = sin(yaw) * cos(pitch)
+    object_pose.orientation.z = sin(pitch)
+    object_pose.orientation.w = 0 
+    
+    quaternion = quaternion_from_euler(-pi/2., 0.0, 0.0)
+    object_pose.orientation.x = quaternion[0]
+    object_pose.orientation.y = quaternion[1]
+    object_pose.orientation.z = quaternion[2]
+    object_pose.orientation.w = quaternion[3]
+        
+    grasper.move_tip_absolute(object_pose)
+    time.sleep(1)
+    
     rospy.loginfo("opening hand")
     grasper.open_hand()
     time.sleep(1)
     
     rospy.loginfo("move tool tip to object pose")
-    grasper.move_tip(x, y, z, roll, pitch, yaw)
+    grasper.move_tip(y=-0.164)
     time.sleep(1)
     print("tip pose 2: {tippose}").format(tippose=grasper.get_tip_pose())
     time.sleep(1)
@@ -48,6 +71,8 @@ def grasp_ball(x, y, z, roll, pitch, yaw):
     for _ in range(10):
         grasper.move_tip(y=0.01)
         time.sleep(0.1)
+        
+    grasper.check_fingers_collisions(True)
         
         
 if __name__ == '__main__':
